780d4ebdc9f196d1354c3fbb72da02f6ec7872fe
Harsh Lathwal <xeon.harsh@gmail.com>
2017-01-27 00:00:52 +0530

    Create monochrome.py

---
+++ monochrome.py
@@ -0,0 +1,9 @@
+# to apply monochrome theme with help of a script
+# The Urban Shadows wallpaper by Paul Scott on Flickr (https://www.flickr.com/photos/49866381@N05/31741299735/in/explore-2016-12-19/)
+# Conky, a lightweight system monitor for Linux systems (http://conky.sourceforge.net/)
+# This custom Conky config (http://speedracker.deviantart.com/art/Loco-Panda-Grace-conky-650071619)
+# The Sardi Icons by Erik Dubois to replace the default icon (http://erikdubois.be/category/sardi-icons/)
+# above data from life hacker (http://lifehacker.com/the-monochrome-arch-desktop-1791434657)
+
+# for cinnamon
+# gsettings set org.cinnamon.desktop.background picture-uri  "file:///filename"
